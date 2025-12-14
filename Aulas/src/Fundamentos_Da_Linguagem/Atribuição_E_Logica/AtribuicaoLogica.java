5532c2c40afc11ba13aa2b3cbbf8a43da74fd2bd
Hellys Fernandes <hellysfernandesrotimei@gmail.com>
2025-12-13 22:09:12 -0300

    Atribuição Logica

--- Aulas/src/Fundamentos_Da_Linguagem/Atribuição_E_Logica/AtribuicaoLogica.java
+++ Aulas/src/Fundamentos_Da_Linguagem/Atribuição_E_Logica/AtribuicaoLogica.java
@@ -7,10 +7,29 @@ public class AtribuicaoLogica {
         Scanner input = new Scanner(System.in);
 
         System.out.println("quanto é 2 + 2");
-        String result = input.next();
-        boolean real = result == "4";
-        System.out.println("o resultado é 4 e voce acertou (" +real+")");
+        int result = input.nextInt();
+        boolean real = result == 4;
+        System.out.println("o resultado é 4 e voce acertou (" + real + ")");
 
+        System.out.println("quantos anos voce tem ?");
+        int idade = input.nextInt();
+        System.out.println("voce é emancipado ?");
+        boolean isEmancipado = input.nextBoolean();
+        boolean drive = idade >= 18 || (isEmancipado && idade >= 16);
+        System.out.println("voce pode dirigir (" + drive + ")");
 
+        System.out.println("====================================");
+        System.out.println("true && true = " + (true && true));
+        System.out.println("true && false = " + (true && false));
+        System.out.println("false && false = " + (false && false));
+        System.out.println("false && true = " + (false && true));
+        System.out.println("====================================");
+        System.out.println("true || true = " + (true || true));
+        System.out.println("true || false = " + (true || false));
+        System.out.println("false || false = " + (false || false));
+        System.out.println("false || true = " + (false || true));
+        System.out.println("=====================================");
+        System.out.println("!true = " + !true);
+        System.out.println("!false = " + !false);
     }
 }
