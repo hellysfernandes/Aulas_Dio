61f45659d0faaa1a883c0f9081e643cba4bcc086
Hellys Fernandes <hellys fernandes>
2026-01-06 10:43:15 -0300

    incrementos do exercisio carro

--- Aulas/src/Programação_Orientada_a_Obijetos/Abstração_com_Classes_e_Encapsulamento/Exercisio2/Carro.java
+++ Aulas/src/Programação_Orientada_a_Obijetos/Abstração_com_Classes_e_Encapsulamento/Exercisio2/Carro.java
@@ -1,7 +1,5 @@
 package Programação_Orientada_a_Obijetos.Abstração_com_Classes_e_Encapsulamento.Exercisio2;
 
-import javax.management.MXBean;
-
 public class Carro {
     private boolean ligado;
     private int marcha;
@@ -103,21 +101,80 @@ public class Carro {
 
     public void diminuirVelocidade() {
         if(ligado == true) {
-            if(marcha > 0) {
+            switch(marcha) {
+                case 1:
+                    if(velocidade >= 1 && velocidade <= 20) {
+                        velocidade -= 1;
+                    } else {
+                        System.out.println("Fail: minimo de velocidade atinjido");
+                    }
+                    break;
 
-                velocidade -= 1;
-            } else {
-                System.out.println("Fail: inposivel de acelerar com a marcha no neutro");
+                case 2:
+                    if(velocidade >= 21 && velocidade <= 40) {
+                        velocidade -= 1;
+                    } else {
+                        System.out.println("Fail: minimo de velocidade atinjido");
+                    }
+                    break;
+
+                case 3:
+                    if(velocidade >= 41 && velocidade <= 60) {
+                        velocidade -= 1;
+                    } else {
+                        System.out.println("Fail: minimo de velocidade atinjido");
+                    }
+                    break;
+
+                case 4:
+                    if(velocidade >= 61 && velocidade <= 80){
+                        velocidade -= 1;
+                    } else {
+                        System.out.println("Fail: minimo de velocidade atinjido");
+                    }
+                    break;
+
+                case 5:
+                    if(velocidade >= 81 && velocidade <= 100){
+                        velocidade -= 1;
+                    } else {
+                        System.out.println("Fail: minimo de velocidade atinjido");
+                    }
+                    break;
+
+                case 6:
+                    if(velocidade >= 101 && velocidade <= 120){
+                        velocidade -= 1;
+                    } else {
+                        System.out.println("Fail: minimo de velocidade atinjido");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Fail: o carro nao pode sair na marcha neutra");
+                    break;
             }
         } else {
             System.out.println("Fail: O Carro presisa ser ligado antes");
         }
     }
 
-    public void trocarMarcha(int marcha) {
+    public void trocarMarcha(int trocaMarcha) {
         if(ligado == true) {
-            if(marcha == 1) {
-
+            if(trocaMarcha == 1) {
+                if(marcha == 0 || (marcha > 0 && marcha < 6)) {
+                    marcha += 1;
+                } else if(marcha == 6){
+                    System.out.println("Fail ja esta na ultima marcha");
+                } else {
+                    System.out.println("Fail: marcha incapas de ser modifcada");
+                }
+            } else {
+                if(marcha > 0) {
+                    marcha -= 1;
+                }else {
+                    System.out.println("Fail: voce ja esta no neutro");
+                }
             }
         } else {
             System.out.println("Fail: O Carro presisa ser ligado antes");
